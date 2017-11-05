013ab16d8fff71ae2a5e7aefd4a15762d119aaf7
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-05 05:35:21 +0000

    Add a test case

--- test/test.js
+++ test/test.js
@@ -1,12 +1,35 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+var supertest = require("supertest");
+var should = require("should");
+
+// var assert = require('assert');
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal(-1, [1,2,3].indexOf(4));
+//     });
+//   });
+// });
 
 //curl -X POST -H "Content-Type: application/json" -d '{"query": "{ hello }"}' http://0.0.0.0:8080/graphql
 //{"data":{"hello":"Hello world!"}}
 
+var server = supertest.agent("http://0.0.0.0:8080");
+
+describe("simple query test",function(){
+  it("should return hello world",function(done){
+    server
+    .post("/graphql")
+    .send({"query": "{ hello }"})
+    .expect("Content-type",/json/)
+    .expect(200) //HTTP response
+    .end(function(err,res){
+      res.status.should.equal(200);
+      res.error.should.equal(false);
+      
+      //console.log(res.body.data);
+      res.body.data.should.containEql({ hello: 'Hello world!' });
+      done();
+    });
+  });
+
+});
