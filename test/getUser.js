1f089712fd87994306986de90b4d39c814dc7f6d
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-07 06:19:15 +0000

    Update token

--- test/getUser.js
+++ test/getUser.js
@@ -2,11 +2,11 @@ var request = require('superagent');
 var should = require("should");
 
 /* global describe it */
-describe("get user profile",function(){
-  it("should return user profile",function(done){
+describe("get current user's name",function(){
+  it("should return current user's name",function(done){
     request
     .post("https://api.github.com/graphql")
-    .set({'Content-Type': 'application/json', 'Authorization': 'Bearer ' + 'f9383a69e3e79cb70abae61780989af73190b820'})
+    .set({'Content-Type': 'application/json', 'Authorization': 'Bearer ' + 'da35ade53992329d78bd46d0d57ffd37e1ed'+'b4bf'})
     .send({"query": "{ viewer { login } }"})
     .end(function(err, res){
         if (err || !res.ok) {
@@ -18,24 +18,22 @@ describe("get user profile",function(){
       done();
     });
   });
-
 });
 
-// describe("simple query test",function(){
-//   it("should return hello world",function(done){
-//     server
-//     .post("/graphql")
-//     .send({"query": "{ hello }"})
-//     .expect("Content-type",/json/)
-//     .expect(200) //HTTP response
-//     .end(function(err,res){
-//       res.status.should.equal(200);
-//       res.error.should.equal(false);
-      
-//       //console.log(res.body.data);
-//       res.body.data.should.containEql({ hello: 'Hello world!' });
-//       done();
-//     });
-//   });
-
-// });
+describe("get current user's name",function(){
+  it("should return current user's name",function(done){
+    request
+    .post("https://api.github.com/graphql")
+    .set({'Content-Type': 'application/json', 'Authorization': 'Bearer ' + 'da35ade53992329d78bd46d0d57ffd37e1ed'+'b4bf'})
+    .send({"query": "{ viewer { login } }"})
+    .end(function(err, res){
+        if (err || !res.ok) {
+            console.log('Found error');
+        } else {
+            //console.log('yay got ' + JSON.stringify(res.body));
+            res.body.data.should.containEql({"viewer":{"login":"hyeri0609"}});
+        }
+      done();
+    });
+  });
+});
