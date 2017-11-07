a8ed2bcd2272d2190cb8551d68465174232698d4
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-07 06:29:37 +0000

    Add more test case

--- test/getUser.js
+++ test/getUser.js
@@ -2,23 +2,8 @@ var request = require('superagent');
 var should = require("should");
 
 /* global describe it */
-describe("get current user's name",function(){
-  it("should return current user's name",function(done){
-    request
-    .post("https://api.github.com/graphql")
-    .set({'Content-Type': 'application/json', 'Authorization': 'Bearer ' + 'da35ade53992329d78bd46d0d57ffd37e1ed'+'b4bf'})
-    .send({"query": "{ viewer { login } }"})
-    .end(function(err, res){
-        if (err || !res.ok) {
-            console.log('Found error');
-        } else {
-            //console.log('yay got ' + JSON.stringify(res.body));
-            res.body.data.should.containEql({"viewer":{"login":"hyeri0609"}});
-        }
-      done();
-    });
-  });
-});
+
+//https://developer.github.com/v4/reference/object/user/
 
 describe("get current user's name",function(){
   it("should return current user's name",function(done){
@@ -27,11 +12,33 @@ describe("get current user's name",function(){
     .set({'Content-Type': 'application/json', 'Authorization': 'Bearer ' + 'da35ade53992329d78bd46d0d57ffd37e1ed'+'b4bf'})
     .send({"query": "{ viewer { login } }"})
     .end(function(err, res){
+        res.status.should.equal(200);
+        res.error.should.equal(false);
         if (err || !res.ok) {
             console.log('Found error');
         } else {
-            //console.log('yay got ' + JSON.stringify(res.body));
             res.body.data.should.containEql({"viewer":{"login":"hyeri0609"}});
+            console.log('res.body: ' + JSON.stringify(res.body));
+        }
+      done();
+    });
+  });
+});
+
+describe("get current user's email",function(){
+  it("should return current user's email",function(done){
+    request
+    .post("https://api.github.com/graphql")
+    .set({'Content-Type': 'application/json', 'Authorization': 'Bearer ' + 'da35ade53992329d78bd46d0d57ffd37e1ed'+'b4bf'})
+    .send({"query": "{ viewer { login avatarUrl email } }"})
+    .end(function(err, res){
+        res.status.should.equal(200);
+        res.error.should.equal(false);
+        if (err || !res.ok) {
+            console.log('Found error');
+        } else {
+            console.log('res.body: ' + JSON.stringify(res.body));
+            res.body.data.should.containEql({"viewer":{"login":"hyeri0609","avatarUrl":"https://avatars1.githubusercontent.com/u/32530272?v=4","email":""}});
         }
       done();
     });
