b8d8dfaf89ec1c5411c4dc95fb9fda41bf64780d
hyeri0609 <hyeri_0609@protonmail.com>
2017-11-07 05:50:10 +0000

    Update bearer in superagent

--- test/getUser.js
+++ test/getUser.js
@@ -1,13 +1,13 @@
-var supertest = require("supertest");
+var request = require('superagent');
 var should = require("should");
 
-var superagent = supertest.agent("https://api.github.com");
-var Request = superagent.Request;
-if (Request) {
-  Request.prototype.bearer = function(token) {
-    this.set('Authorization', 'Bearer ' + token);
-  };
-}
+// var superagent = supertest.agent("https://api.github.com");
+// var Request = request.Request;
+// if (Request) {
+//   Request.prototype.bearer = function(token) {
+//     Request.set('Authorization', 'Bearer ' + token);
+//   };
+// }
 
 // query {
 //   __schema {
@@ -26,12 +26,14 @@ if (Request) {
 describe("get user profile",function(){
   it("should return user profile",function(done){
       //superagent.Request.set('Authorization', 'Bearer 33e0802256a91eb0c00e9cf6c45d82522753d6a9');
-    superagent
-    .get("/graphql")
-    .bearer('33e0802256a91eb0c00e9cf6c45d82522753d6a9')
-    .set('Accept', 'application/json')
+    request
+    .post("https://api.github.com/graphql")
+    .set({'Content-Type': 'application/json', 'Authorization': 'Bearer ' + '33e0802256a91eb0c00e9cf6c45d82522753d6a9'})
+    //.bearer('33e0802256a91eb0c00e9cf6c45d82522753d6a9')
+    //.set('Authorization', 'Bearer 33e0802256a91eb0c00e9cf6c45d82522753d6a9')
+    //.set('Accept', 'application/json')
     .send({"query": "{ query {  __schema {    types {      name      kind      description      fields {        name      }    }  }} }"})
-    .end(function(err,res){
+    .end(function(err, res){
       res.status.should.equal(200);
       res.error.should.equal(false);
       
@@ -43,21 +45,21 @@ describe("get user profile",function(){
 
 });
 
-describe("simple query test",function(){
-  it("should return hello world",function(done){
-    server
-    .post("/graphql")
-    .send({"query": "{ hello }"})
-    .expect("Content-type",/json/)
-    .expect(200) //HTTP response
-    .end(function(err,res){
-      res.status.should.equal(200);
-      res.error.should.equal(false);
+// describe("simple query test",function(){
+//   it("should return hello world",function(done){
+//     server
+//     .post("/graphql")
+//     .send({"query": "{ hello }"})
+//     .expect("Content-type",/json/)
+//     .expect(200) //HTTP response
+//     .end(function(err,res){
+//       res.status.should.equal(200);
+//       res.error.should.equal(false);
       
-      //console.log(res.body.data);
-      res.body.data.should.containEql({ hello: 'Hello world!' });
-      done();
-    });
-  });
+//       //console.log(res.body.data);
+//       res.body.data.should.containEql({ hello: 'Hello world!' });
+//       done();
+//     });
+//   });
 
-});
+// });
